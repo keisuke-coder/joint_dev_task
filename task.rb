e0aee6ecb99b94f15783dffcb064e7d4cc5d2bd7
keisuke <k.kanno.rkt@gmail.com>
2020-11-08 11:54:12 +0900

    Change q8-q11

--- task.rb
+++ task.rb
@@ -63,12 +63,10 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map {
-    |p| p.capitalize
-  }
-  upper_case_programming_languages = programming_languages.map {
-    |p| p.upcase
-  }
+  programming_languages.map!(&:capitalize)
+  
+  upper_case_programming_languages = programming_languages.map(&:upcase)
+
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -78,8 +76,8 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.each_with_index do |n, i|
-    puts "会員No.#{i+1} #{n}さん"
+  names.each.with_index(1) do |name, index|
+    puts "会員No.#{index} #{name}さん"
   end
 end
 
@@ -87,8 +85,8 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  foods.each do |f|
-    if f.include?("うに")
+  foods.each do |food|
+    if food.include?("うに")
       puts "好物です"
     else
       puts "まぁまぁ好きです"
@@ -100,10 +98,9 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports.flatten!
-  sports.uniq!
-  sports.each_with_index do |s, i|
-    puts "No#{i + 1} #{s}"
+  sports.flatten!.uniq!
+  sports.each.with_index(1) do |s, index|
+    puts "No#{index} #{s}"
   end
 end
 
