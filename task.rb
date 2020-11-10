6cc6b20c67a0cb19f19831ccc152b9f1cd170209
keisuke <k.kanno.rkt@gmail.com>
2020-11-10 15:31:02 +0900

    Answer Q17-Q19

--- task.rb
+++ task.rb
@@ -154,7 +154,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
-
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+  
+  def info
+    puts <<~EOS
+    名前:#{@name}
+    年齢:#{@age}
+    性別:#{@gender}
+    EOS
+  end
 end
 
 def q17
@@ -169,7 +181,18 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
 
+  def introduce
+    if @age >= 18
+      return "こんにちは,#{@name}と申します。よろしくお願いいたします。"
+    else
+      return "はいさいまいど〜,#{@name}です!!!"
+    end
+  end
 end
 
 def q18
@@ -183,9 +206,12 @@ end
 
 class Item
   # 以下を修正して下さい
-
-  def initialize(name)
+  def initialize(name:)
     @name = name
+  end
+
+  def name
+    return @name
   end
 end
 
